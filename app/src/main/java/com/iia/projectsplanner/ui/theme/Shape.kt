616bc3f582d5efe267a98c56e2a1a5485664e1a2
mubarakadem <mubarakadem8@gmail.com>
2022-03-15 06:54:17 +0300

    Shape object added

---
+++ app/src/main/java/com/iia/projectsplanner/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.iia.projectsplanner.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object Shape{
+    val small = RoundedCornerShape(4.dp)
+    val medium = RoundedCornerShape(8.dp)
+    val large = RoundedCornerShape(16.dp)
+}
